250c37e139f8bcb614ca65888ebdaa95a58707bc
HarmonyMorse <harmonythedev@gmail.com>
2024-01-09 11:21:25 -0800

    Add weavers index page and URL mapping

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('weavers/', views.weavers_index, name='index')
 ]
