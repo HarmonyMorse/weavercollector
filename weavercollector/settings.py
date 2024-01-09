852b794ba68255112539efdd8bfbcfcaf3089792
HarmonyMorse <harmonythedev@gmail.com>
2024-01-09 09:55:35 -0800

    Update database name in settings.py

--- weavercollector/settings.py
+++ weavercollector/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'weavercollector.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'weavercollector',
     }
 }
 
