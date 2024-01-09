7f480c8ed6cbeaf109a30293247fbe9d49139e9e
HarmonyMorse <harmonythedev@gmail.com>
2024-01-09 11:03:53 -0800

    Add 'about' path to urlpatterns

--- main_app/views.py
+++ main_app/views.py
@@ -23,4 +23,7 @@ weavers = [
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'weavers': weavers})
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
