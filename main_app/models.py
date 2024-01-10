8ecbb1be9f6711437b6660e459eab12ac6194716
HarmonyMorse <harmonythedev@gmail.com>
2024-01-10 07:42:54 -0800

    Add Weaver model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Weaver(models.Model):
+    alias = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    universe = models.CharField(max_length=50)
+    enemies = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
