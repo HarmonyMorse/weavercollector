7f145e3210e22b4a9f0a45270c1211b7483e0748
HarmonyMorse <harmonythedev@gmail.com>
2024-01-11 13:41:08 -0800

    Add create weaver route in urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('weavers/', views.weavers_index, name='index'),
     path('weavers/<int:weaver_id>/', views.weavers_detail, name='detail'),
+    path('weavers/create/', views.WeaverCreate.as_view(), name='weavers_create'),
 ]
