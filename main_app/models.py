bb30744c4e40a4bef0f26cf02764f1c71832cb9d
HarmonyMorse <harmonythedev@gmail.com>
2024-01-11 13:49:56 -0800

    Add get_absolute_url method to Weaver model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Weaver(models.Model):
@@ -9,3 +11,6 @@ class Weaver(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"weaver_id": self.id})
