5910ebb003918356f56be5318de92e6b6d7bd940
HarmonyMorse <harmonythedev@gmail.com>
2024-01-11 13:42:51 -0800

    Add WeaverCreate view with placeholder for enemies field

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.shortcuts import render
 from .models import Weaver
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 def home(request):
@@ -18,3 +20,13 @@ def weavers_detail(request, weaver_id):
     w = Weaver.objects.get(id=weaver_id)
     w.enemies = w.enemies.split(",")
     return render(request, 'weavers/detail.html', {"w": w})
+
+class WeaverCreate(CreateView):
+    model = Weaver
+    fields = '__all__'
+    
+    # https://stackoverflow.com/questions/52021136/is-it-possible-to-add-placeholder-in-generic-create-view-form
+    def get_form(self, form_class=None):
+        form = super(WeaverCreate, self).get_form(form_class)
+        form.fields['enemies'].widget = forms.TextInput(attrs={'placeholder': 'Seperated by commas'})
+        return form
