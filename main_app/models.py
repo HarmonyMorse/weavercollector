b25a24918fde9f0c9bf424ec69b58fda6274012d
HarmonyMorse <harmonythedev@gmail.com>
2024-01-15 21:42:39 -0800

    Add powers field to Weaver model

--- main_app/models.py
+++ main_app/models.py
@@ -2,12 +2,25 @@ from django.db import models
 from django.urls import reverse
 
 
+class Power(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    # Add other Power-related attributes as needed
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("powers_detail", kwargs={"pk": self.id})
+
+
 # Create your models here.
 class Weaver(models.Model):
     alias = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     universe = models.CharField(max_length=50)
     enemies = models.TextField(max_length=250)
+    powers = models.ManyToManyField(Power)
 
     def __str__(self):
         return self.name
@@ -27,15 +40,3 @@ class Sighting(models.Model):
 
     class Meta:
         ordering = ["-date"]
-
-
-class Power(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    # Add other Power-related attributes as needed
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("powers_detail", kwargs={"pk": self.id})
