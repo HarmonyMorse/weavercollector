3f410a8414cf425298d12815785d430e31275d96
HarmonyMorse <harmonythedev@gmail.com>
2024-01-15 21:12:37 -0800

    Add power-related URLs to main_app/urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('weavers/<int:pk>/update/', views.WeaverUpdate.as_view(), name='weavers_update'),
     path('weavers/<int:pk>/delete/', views.WeaverDelete.as_view(), name='weavers_delete'),
     path('weavers/<int:weaver_id>/add_sighting/', views.add_sighting, name='add_sighting'),
+    path('powers/', views.PowerList.as_view(), name='powers_index'),
+    path('powers/<int:pk>/', views.PowerDetail.as_view(), name='powers_detail'),
+    path('powers/create/', views.PowerCreate.as_view(), name='powers_create'),
+    path('powers/<int:pk>/update/', views.PowerUpdate.as_view(), name='powers_update'),
+    path('powers/<int:pk>/delete/', views.PowerDelete.as_view(), name='powers_delete'),
 ]
