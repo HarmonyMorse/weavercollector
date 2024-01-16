5426e3a405ddd577d4e76a96149a2666511f1e50
HarmonyMorse <harmonythedev@gmail.com>
2024-01-15 21:11:59 -0800

    Add Power model to admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Weaver, Sighting
+from .models import Weaver, Sighting, Power
 
 # Register your models here.
 admin.site.register(Weaver)
 admin.site.register(Sighting)
+admin.site.register(Power)
