c4bf6336659cdf2fb4fbb980d29376f24278fe32
HarmonyMorse <harmonythedev@gmail.com>
2024-01-15 21:12:31 -0800

    Add Power model to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -19,11 +19,23 @@ class Weaver(models.Model):
 class Sighting(models.Model):
     date = models.DateField()
     location = models.CharField(max_length=100)
-    
+
     weaver = models.ForeignKey(Weaver, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"{self.location} on {self.date}"
-    
+
     class Meta:
-        ordering = ['-date']
+        ordering = ["-date"]
+
+
+class Power(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    # Add other Power-related attributes as needed
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("powers_detail", kwargs={"pk": self.id})
