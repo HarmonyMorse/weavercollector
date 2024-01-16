93d83d96d2e3eb58b52a3a62a11cd05bfd34aeea
HarmonyMorse <harmonythedev@gmail.com>
2024-01-15 20:31:19 -0800

    Add 'add_sighting' path to weavers URLs

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('weavers/create/', views.WeaverCreate.as_view(), name='weavers_create'),
     path('weavers/<int:pk>/update/', views.WeaverUpdate.as_view(), name='weavers_update'),
     path('weavers/<int:pk>/delete/', views.WeaverDelete.as_view(), name='weavers_delete'),
+    path('weavers/<int:weaver_id>/add_sighting/', views.add_sighting, name='add_sighting'),
 ]
